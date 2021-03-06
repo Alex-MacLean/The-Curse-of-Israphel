cc5a9d3205737b5d681a609e1e00009b63c7a8c6
Alex MacLean <macleanalex78@gmail.com>
2021-03-06 12:58:47 -0500

    Updated to 1.1

--- src/main/java/com/github/alexmaclean/israphel/Israphel.java
+++ src/main/java/com/github/alexmaclean/israphel/Israphel.java
@@ -10,11 +10,12 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-@Mod(name = Israphel.NAME, useMetadata = true, acceptedMinecraftVersions = Israphel.MCVERSION, version = Israphel.VERSION, modid = Israphel.MODID)
+@Mod(name = Israphel.NAME, useMetadata = true, updateJSON = Israphel.UPDATEJSON,acceptedMinecraftVersions = Israphel.MCVERSION, version = Israphel.VERSION, modid = Israphel.MODID)
 public class Israphel {
     public static final String NAME = "The Curse of Israphel";
+    public static final String UPDATEJSON = "https://raw.githubusercontent.com/Alex-MacLean/The-Curse-of-Israphel/main/update.json";
     public static final String MCVERSION = "[1.12.2]";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String MODID = "israphel";
 
     @Contract("_ -> new")
